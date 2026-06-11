5e201c04e215c292c3662e6e943edba0947a0670
Eric Beuque <eric.beuque@gmail.com>
2026-06-11 11:13:59 +0200

    Fix menu account menu

--- ui/src/layout/AppHeader.js
+++ ui/src/layout/AppHeader.js
@@ -1,7 +1,8 @@
 import React, {Fragment, useContext} from 'react';
 import { Link } from "react-router-dom";
+import i18n from '../utils/i18n.js';
 
-import { Menu, Dropdown, Space, Switch, Avatar, Grid, Popover } from 'antd';
+import {  Dropdown, Space, Avatar, Grid } from 'antd';
 import {
     HomeOutlined,
     UserOutlined,
@@ -57,48 +58,58 @@ const AppHeader = () => {
         return "NONE";*/
     }
 
-    // Menu account
-    const menuAccount = (
-        <Menu>
-            {shortMode ? 
-                <Fragment>
-                    <Menu.Item key="menu-account-username">{user ? user.getUsername() : ""}</Menu.Item>
-                    <Menu.Divider />
-                </Fragment>
-            : ""}
-            <Menu.Item key="menu-account-myaccount">
-                <Link to={"/account/edit"}>{"My_Account"}</Link>
-            </Menu.Item>
-            <Menu.Item key="menu-account-logout">
-                <Link to={"/logout"}>{"Logout"}</Link>
-            </Menu.Item>
-        </Menu>
-    );
+
+	function getMenuAccount()
+	{
+		let items = [];
+
+		if(shortMode){
+			items.push({
+				key: 'menu-account-username',
+				label: user.getUsername(),
+				disabled: true
+			});
+			items.push({
+				type: 'divider'
+			});
+		}
+
+		items.push({
+			key: 'menu-account-myaccount',
+			label: <Link to={"/account/edit"}>{i18n.t("menu_account.my_account")}</Link>,
+		});
+		items.push({
+			key: 'menu-account-username',
+			label: <Link to={"/logout"}>{i18n.t("menu_account.logout")}</Link>,
+		});
+
+		return items;
+	}
+
+	function getDropdownMenu(user)
+	{
+		if(!user){
+			return null;
+		}
+
+		const items = getMenuAccount();
+		return (
+			<Dropdown menu={{ items }} placement="bottomRight" trigger={dropdownTrigger} >
+				<a className="app-header-dropdown-link" onClick={e => e.preventDefault()}>
+					{shortMode ? "" : user.getUsername()} <UserOutlined /><CaretDownOutlined />
+				</a>
+			</Dropdown>
+		);
+	}
 
     return (
         <div>
-        <Space size={"large"} className="app-header-right" style={{ padding: 0 }}>
-            {getBreakpointName()}
-            {userIsLogged ?
-                <Fragment>
-                    {user != null ?
-                        <Dropdown overlay={menuAccount} placement="bottomRight" trigger={dropdownTrigger} >
-                            <a className="app-header-dropdown-link" onClick={e => e.preventDefault()}>
-                                {shortMode ? "" : user.getUsername()} <UserOutlined /><CaretDownOutlined />
-                            </a>
-                        </Dropdown>
-                        : ""
-                    }
-                </Fragment>
-            : 
-                <Fragment />
-            }
-        </Space>
+	        <Space size={"large"} className="app-header-right" style={{ padding: 0 }}>
+	            {getBreakpointName()}
+	            {userIsLogged ? getDropdownMenu(user) : null }
+	        </Space>
         </div>
     );
 }
-
-// 
-<Avatar shape="square" size="small" icon={<UserOutlined />} />
 
 export default AppHeader;
